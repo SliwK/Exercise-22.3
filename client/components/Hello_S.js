e54566da1dfb5be77f0cb02b3b44d0f448489d4b
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-03-11 15:00:11 +0100

    Create project

---
+++ client/components/Hello_S.js
@@ -0,0 +1,8 @@
+import React, { PropTypes } from 'react';
+
+const Hello_S = (props, context) => {}
+
+Hello_S.propTypes = {
+};
+
+export default Hello_S;
---
+++ components/Hello_M/Hello_M.js
@@ -0,0 +1,8 @@
+import React, { PropTypes } from 'react';
+
+const Hello_M = (props, context) => {}
+
+Hello_M.propTypes = {
+};
+
+export default Hello_M;
